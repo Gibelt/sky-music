33e2fbe32298cb72107b9782cb3019a45ace2dde
Игорь Опарин <Igorop3@gmail.com>
2023-01-25 15:34:35 +0800

    add-player

--- src/components/bar/Bar.jsx
+++ src/components/bar/Bar.jsx
@@ -1,18 +1,44 @@
+import { useRef, useState } from 'react'
 import PlayerControls from '../playerControls/PlayerControls'
 import TrackPlayerContain from '../trackPlayerContain/TrackPlayerContain'
 import LikeDislike from '../likeDislike/LikeDislike'
 import Volume from '../volume/Volume'
+import BarProgress from '../barProgress/BarProgress'
 import SkeletonPlayerContain from '../skeletons/skeletonPlayerContain/SkeletonPlayerContain'
 import s from './Bar.module.css'
+/* eslint-disable jsx-a11y/media-has-caption */
 
 export default function Bar(props) {
+  const playControls = useRef(null)
+
+  const [isPlay, setPlay] = useState(true)
+  const togglePlay = () => setPlay(!isPlay)
+
+  const [id, setId] = useState(null)
+
+  const [width, setWidth] = useState(0)
+
+  const playTrack = () => {
+    togglePlay();
+    if(isPlay) {
+      playControls.current.play();
+      setId(setInterval(() => {setWidth(c => c + 1 / (playControls.current.duration / 50))}, 500));
+    } else {
+      setId(clearInterval(id));
+      playControls.current.pause();
+    }    
+   }
+
   return (
     <div className={s.bar}>
+      <audio controls ref={playControls}>
+        <source src="../../Bobby_Marleni_-_Dropin.mp3" type="audio/mp3" />
+      </audio>
       <div className={s.content}>
-        <div className={s.progress} />
+        <BarProgress width={width} />
         <div className={s.block}>
           <div className={s.player}>
-            <PlayerControls />
+            <PlayerControls isPlay={playTrack} />
             <div className={s.track__play}>
               {props.loader ? (
                 <SkeletonPlayerContain />
---
+++ src/components/barProgress/BarProgress.jsx
@@ -0,0 +1,16 @@
+import s from './BarProgress.module.css'
+
+export default function BarProgress(props) {
+
+  const color = {
+    width: `${props.width}%`,
+    backgroundColor: 'red',
+    height: 5,
+  }
+
+  return (
+    <div className={s.progress}>
+      <div style={color} />
+    </div>
+  )
+}
